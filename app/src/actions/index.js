709bcc260b2a01fc7c1c072d5ea8d94f2006105e
Bryce Slade <bryceslade@gmail.com>
2021-11-03 14:19:40 -0600

    finished

---
+++ app/src/actions/index.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_START = 'FETCH_START';
+export const FETCH_ERROR = 'FETCH_ERROR';
+
+export const getJoke = () => {
+    return (dispatch) => {
+        dispatch(fetchStart());
+        axios.get('https://v2.jokeapi.dev/joke/Any')
+            .then(response => {
+                console.log(response)
+                dispatch(fetchSuccess(response.data))
+            })
+            .catch(error => {
+                dispatch(fetchError(error))
+            });
+    }
+}
+
+export const fetchStart = () => {
+    return({type: FETCH_START});
+}
+
+export const fetchSuccess = (joke) => {
+    return({type: FETCH_SUCCESS, payload: joke});
+}
+
+export const fetchError = (errorMessage) => {
+    return({type: FETCH_ERROR, payload: errorMessage});
+}
--- app/src/index.js
+++ app/src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { reducer } from './reducers'
 
+// https://pokeapi.co/api/v2/
+
+const store = createStore(reducer, applyMiddleware(thunk));
+
+const rootElement = document.getElementById('root');
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  // </React.StrictMode>,
+  rootElement
 );
 
 // If you want to start measuring performance in your app, pass a function
